1721a8b75d1b6c57f04c128c04b676d7bcb601de
jkearney <josephkearney14@gmail.com>
2020-07-01 19:43:00 -0400

    updated formatter

--- pg2kinesis/formatter.py
+++ pg2kinesis/formatter.py
@@ -10,7 +10,7 @@ from collections import namedtuple
 # Tuples representing changes as pulled from database
 Change = namedtuple('Change', 'xid, table, operation, pkey')
 FullChange = namedtuple('FullChange', 'xid, change')
-FullChange.operation = property(lambda self: self.change["row"].get('kind'))
+FullChange.operation = property(lambda self: self.change.get('kind'))
 
 # Final product of Formatter, a Change and the Change formatted.
 Message = namedtuple('Message', 'change, fmt_msg')
@@ -114,10 +114,26 @@ class Formatter(object):
             schema = change['schema']
             if self.table_re.search(table_name):
                 if self.full_change:
+                    event_mapping = {
+                        'insert': 'WriteRowsEvent',
+                        'update': 'UpdateRowsEvent',
+                        'delete': 'DeleteRowsEvent'
+                    }
                     d = {}
+
+                    # create root keys
                     d["xid"] = self.cur_xact
                     d["timestamp"] = change_dictionary["timestamp"]
+                    d["kind"] = change["kind"]
+                    d["type"] = event_mapping[change["kind"]]
+                    d["schema"] = change["schema"]
+                    d["table"] = change["table"]
                     d["row"] = change
+
+                    # pop some info
+                    d["row"].pop("kind")
+                    d["row"].pop("schema")
+                    d["row"].pop("table")
                     changes.append(FullChange(xid=self.cur_xact, change=d))
                 else:
                     try:
